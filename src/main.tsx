2945fc2a2a319643968e55c52458149d1dee3144
Jan <jan.mueller02@sap.com>
2026-01-14 11:42:32 +0100

    einbinden von keycloak

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,36 @@
   import App from "./App.tsx";
   import "./index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+import React from "react";
+//import ReactDOM from "react-dom/client";
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import keycloak from "./auth/keycloak";
+
+const onKeycloakEvent = (event: unknown, error?: unknown) => {
+  // optional: Logging
+  // console.log("Keycloak event", event, error);
+};
+
+const onKeycloakTokens = (tokens: unknown) => {
+  // optional: Debugging
+  // console.log("Keycloak tokens", tokens);
+};
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ReactKeycloakProvider
+      authClient={keycloak}
+      initOptions={{
+        onLoad: "check-sso", // oder "login-required"
+        pkceMethod: "S256",
+        checkLoginIframe: false, // oft stabiler in SPAs
+      }}
+      onEvent={onKeycloakEvent}
+      onTokens={onKeycloakTokens}
+    >
+      <App />
+    </ReactKeycloakProvider>
+  </React.StrictMode>
+);
+
   
